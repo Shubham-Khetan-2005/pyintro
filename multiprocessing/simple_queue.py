f40d7c265da4bcf150f196904842ca4ffde6b7ab
Rafael Lopes <rafael.lopes@acotel.com>
2013-04-10 10:10:09 -0300

    Melhora logs para facilitar entendimento da lógica das threads.

--- multiprocessing/simple_queue.py
+++ multiprocessing/simple_queue.py
@@ -1,3 +1,4 @@
+import time
 import random
 import logging
 import threading
@@ -18,6 +19,7 @@ class Producer(threading.Thread):
     def run(self):
         for item in range(random.randint(5, 20)):
             logging.debug('producing %s' % item)
+            time.sleep(random.randint(0, 2))
             self.queue.put(item)
         logging.debug('done')
 
@@ -39,23 +41,31 @@ queue = Queue()
 producers = [Producer(queue) for x in range(10)]
 consumers = [Consumer(queue) for x in range(5)]
 
+logging.debug('starting producers...')
 counter = 1
 for producer in producers:
     producer.setName('producer #%02d' % counter)
     producer.setDaemon(True)
     producer.start()
     counter += 1
+logging.debug('...producers up and running')
 
+logging.debug('starting consumers...')
 counter = 1
 for consumer in consumers:
     consumer.setName('consumer #%02d' % counter)
     consumer.setDaemon(True)
     consumer.start()
     counter += 1
+logging.debug('...consumers up and running')
 
+logging.debug('waiting producers...')
 for producer in producers:
     producer.join()
+logging.debug('...producers done')
 
+logging.debug('waiting queue...')
 queue.join()
+logging.debug('...queue done')
 
-print 'Program terminated'
+logging.debug('program terminated')
